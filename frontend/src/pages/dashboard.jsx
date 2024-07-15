774b0d232b3cf0187082fa89291503280ca89ad0
Hadi Youssef <hmyoussef@gmail.com>
2024-07-15 16:55:58 -0400

    Added functionality to ensure the user doesn't accidentally delete their account

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,18 +1,27 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { UserContext } from '../App';
 
-function Dashboard({handleLogOut}) {
+function Dashboard({ handleLogOut }) {
+    const [enterError, setEnterError] = useState(false)
     const navigate = useNavigate();
+    const [modal, setModal] = useState(false);
     const { user, loggedIn, setUser, setLoggedIn } = useContext(UserContext);
     const location = useLocation();
     const username = location.state ? location.state.username : '';
-    async function handleDelete(){
-        try{
+    const [enteredUser, setEnteredUser] = useState('');
+
+
+    const handleModalToggle = () => {
+        setModal(!modal); 
+    };
+
+    async function handleDelete() {
+        try {
             const deleteUser = await fetch("http://localhost:3000/deleteUser", {
                 method: "DELETE",
                 headers: {
-                    "Content-Type":'application/json'
+                    "Content-Type": 'application/json'
                 },
                 credentials: "include",
                 body: JSON.stringify({
@@ -20,23 +29,22 @@ function Dashboard({handleLogOut}) {
                 })
             });
             console.log(deleteUser)
-            if(deleteUser.ok) {
-                const logOut = await fetch("http//localhost:3000/logout", {
+            if (deleteUser.ok) {
+                const logOut = await fetch("http://localhost:3000/logout", {
                     method: "GET",
                     headers: {
                         "Content-Type": "application/json"
                     }
                 });
-                if(logOut.ok) {
+                if (logOut.ok) {
                     console.log("User Deleted!")
-                    handleLogOut()
-                    navigate('/')
-                }
-                else{
-                    console.error("There was a problem")
+                    handleLogOut();
+                    navigate('/');
+                } else {
+                    console.error("There was a problem");
                 }
             }
-        } catch(error){
+        } catch (error) {
             console.error(error);
         }
     }
@@ -49,12 +57,53 @@ function Dashboard({handleLogOut}) {
 
     return (
         <>
-            {loggedIn ? <>
-                <h1>Welcome {user}</h1> 
-                <button className="btn btn-primary" onClick={handleDelete}>Delete Account</button>
-                <button className="btn btn-primary" onClick={handleLogOut}>Logout</button>
-            </>
-            : null}
+            {loggedIn ? (
+                <>
+                {!modal? (
+                <>
+                    <h1>Welcome {user}</h1>
+                    <button className="btn btn-primary" onClick={handleModalToggle}>
+                        Delete Account
+                    </button>
+                    <button className="btn btn-primary" onClick={handleLogOut}>
+                        Logout
+                    </button>
+                </>
+                ): (
+                        <div className="card">
+                            <div className="card-content">
+                                <h1>Are you sure you want to delete your account?</h1>
+                                <h5>Please enter your username below and press "confirm" if you do</h5>
+                                <div className="form-group">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="username"
+                                        placeholder="Username"
+                                        value={enteredUser}
+                                        onChange={(e) => setEnteredUser(e.target.value)}
+                                    />
+                                </div>
+                                <button className="btn btn-primary" onClick={()=>{
+                                    if(user == enteredUser){
+                                        handleDelete()
+                                    }
+                                    else{
+                                        setEnterError(true)
+                                    }
+                                }
+                                }>
+                                    Confirm
+                                </button>
+                                <button className="btn btn-secondary" onClick={handleModalToggle}>
+                                    Cancel
+                                </button>
+                                {enterError ? (<h4 className="error-message">You entered your username wrong</h4>):(null)}
+                            </div>
+                        </div>
+                    )}
+                </>
+            ) : null}
         </>
     );
 }
